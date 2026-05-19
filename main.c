690a510297bc1c7ae5741f0fb5d1611dbe2218d6
ThousandYearGrind <patrickd9900@gmail.com>
2026-05-18 20:32:33 -0500

    refactor for snake case

--- main.c
+++ main.c
@@ -24,15 +24,15 @@ enum colors {white, black};
 // bit manipulation section
 
 // operation at bit[square] in the bitboard
-static inline U64 setBit(U64 *bitboard, int square) {
+static inline U64 set_bit(U64 *bitboard, int square) {
     return (*bitboard) |= (1ULL << square);
 }
 
-static inline U64 getBit(U64 bitboard, int square) {
+static inline U64 get_bit(U64 bitboard, int square) {
     return bitboard & (1ULL << square);
 }
 
-static inline U64 clearBit(U64 *bitboard, int square) {
+static inline U64 clear_bit(U64 *bitboard, int square) {
     return (*bitboard) &= ~(1ULL << square);
 }
 
@@ -40,14 +40,14 @@ static inline int sq(int rank, int file) {
     return (rank << 3) + file;
 }
 
-void printBitboard(U64 bitboard) {
+void print_bitboard(U64 bitboard) {
     for (int rank = 0; rank < 8; ++rank) {
         for (int file = 0; file < 8; ++file) {
             int square = sq(rank, file); // square index
             // print ranks
             if (!file)
                 printf("  %d ", 8 - rank);
-            printf(" %d", getBit(bitboard, square) ? 1 : 0);
+            printf(" %d", get_bit(bitboard, square) ? 1 : 0);
         }
         printf("\n");
     }
@@ -60,64 +60,60 @@ void printBitboard(U64 bitboard) {
 // attacks section
 
 // file masks
-const U64 notFileA = 18374403900871474942ULL;
-const U64 notFileB = 18302063728033398269ULL;
-const U64 notFileC = 18157383382357244923ULL;
-const U64 notFileD = 17868022691004938231ULL;
-const U64 notFileE = 17289301308300324847ULL;
-const U64 notFileF = 16131858542891098079ULL;
-const U64 notFileG = 13816973012072644543ULL;
-const U64 notFileH = 9187201950435737471ULL;
-const U64 notFileAB = notFileA & notFileB;
-const U64 notFileGH = notFileG & notFileH;
+const U64 not_file_a = 18374403900871474942ULL;
+const U64 not_file_b = 18302063728033398269ULL;
+const U64 not_file_g = 13816973012072644543ULL;
+const U64 not_file_h = 9187201950435737471ULL;
+const U64 not_file_ab = not_file_a & not_file_b;
+const U64 not_file_gh = not_file_g & not_file_h;
 
 // pawn attacks table [color][square]
-U64 pawnAttacksTable[2][64];
+U64 pawn_attacks_table[2][64];
 // knight attacks table [square]
-U64 knightAttacksTable[64];
+U64 knight_attacks_table[64];
 // king attacks table [square]
-U64 kingAttacksTable[64];
+U64 king_attacks_table[64];
 // bishops attacks table [square]
-U64 bishopAttacksTable[64];
+U64 bishop_attacks_table[64];
 
 // generate pawn attacks
-U64 maskPawnAttacks(int square, int color) {
+U64 mask_pawn_attacks(int square, int color) {
     // result attacks bitboard
     U64 attacks = 0ULL;
     // piece bitboard
     U64 bitboard = 0ULL;
     // set piece on board
-    setBit(&bitboard, square);
+    set_bit(&bitboard, square);
     // white pawns
     if (color == white) {
-        attacks |= (bitboard & notFileH) >> 7;
-        attacks |= (bitboard & notFileA) >> 9;
+        attacks |= (bitboard & not_file_h) >> 7;
+        attacks |= (bitboard & not_file_a) >> 9;
     }
     // black pawns
     else {
-        attacks |= (bitboard & notFileH) << 9;
-        attacks |= (bitboard & notFileA) << 7;
+        attacks |= (bitboard & not_file_h) << 9;
+        attacks |= (bitboard & not_file_a) << 7;
     }
     return attacks;
 }
 
 // generate knight attacks
-U64 maskKnightAttacks(int square) {
+U64 mask_knight_attacks(int square) {
     U64 attacks = 0ULL;
     U64 bitboard = 0ULL;
-    setBit(&bitboard, square);
-    if (bitboard & notFileH) {
+    set_bit(&bitboard, square);
+    if (bitboard & not_file_h) {
         attacks |= (bitboard << 17); // down 2 right 1
         attacks |= (bitboard >> 15); // up 2 right 1
-        if (bitboard & notFileGH) {
+        if (bitboard & not_file_gh) {
             attacks |= (bitboard << 10); // down 1 right 2
             attacks |= (bitboard >> 6); // up 1 right 2
         }
     }
-    if (bitboard & notFileA) {
+    if (bitboard & not_file_a) {
         attacks |= (bitboard << 15); // down 2 left 1
         attacks |= (bitboard >> 17); // up 2 left 1
-        if (bitboard & notFileAB) {
+        if (bitboard & not_file_ab) {
             attacks |= (bitboard >> 10); // up 1 left 2
             attacks |= (bitboard << 6); // down 1 left 2
         }
@@ -126,16 +122,16 @@ U64 maskKnightAttacks(int square) {
 }
 
 // generate king attacks
-U64 maskKingAttacks(int square) {
+U64 mask_king_attacks(int square) {
     U64 attacks = 0ULL;
     U64 bitboard = 0ULL;
-    setBit(&bitboard, square);
-    if (bitboard & notFileH) {
+    set_bit(&bitboard, square);
+    if (bitboard & not_file_h) {
         attacks |= bitboard << 9;
         attacks |= bitboard << 1;
         attacks |= bitboard >> 7;
     }
-    if (bitboard & notFileA) {
+    if (bitboard & not_file_a) {
         attacks |= bitboard >> 9;
         attacks |= bitboard >> 1;
         attacks |= bitboard << 7;
@@ -149,16 +145,16 @@ U64 maskKingAttacks(int square) {
  *
  * square - the square that the bishop is on
  */
-U64 maskBishopAttacks(int square) {
+U64 mask_bishop_attacks(int square) {
     U64 attacks = 0ULL;
     int curRank = square / 8;
     int curFile = square % 8;
 
     int r, f;
-    for (r = curRank + 1, f = curFile + 1; r <= 6 && f <= 6; ++r, ++f) setBit(&attacks, sq(r, f));
-    for (r = curRank + 1, f = curFile - 1; r <= 6 && f >= 1; ++r, --f) setBit(&attacks, sq(r, f));
-    for (r = curRank - 1, f = curFile + 1; r >= 1 && f <= 6; --r, ++f) setBit(&attacks, sq(r, f));
-    for (r = curRank - 1, f = curFile - 1; r >= 1 && f >= 1; --r, --f) setBit(&attacks, sq(r, f));
+    for (r = curRank + 1, f = curFile + 1; r <= 6 && f <= 6; ++r, ++f) set_bit(&attacks, sq(r, f));
+    for (r = curRank + 1, f = curFile - 1; r <= 6 && f >= 1; ++r, --f) set_bit(&attacks, sq(r, f));
+    for (r = curRank - 1, f = curFile + 1; r >= 1 && f <= 6; --r, ++f) set_bit(&attacks, sq(r, f));
+    for (r = curRank - 1, f = curFile - 1; r >= 1 && f >= 1; --r, --f) set_bit(&attacks, sq(r, f));
 
     return attacks;
 }
@@ -168,27 +164,27 @@ U64 maskBishopAttacks(int square) {
  * square - the square that the bishop is on
  * board - the pieces on the board around the bishop
  */
-U64 generateBishopAttacks(int square, U64 board) {
+U64 generate_bishop_attacks(int square, U64 board) {
     U64 attacks = 0ULL;
     int curRank = square / 8;
     int curFile = square % 8;
 
     int r, f;
     for (r = curRank + 1, f = curFile + 1; r <= 7 && f <= 7; ++r, ++f) {
-        setBit(&attacks, sq(r, f));
-        if (getBit(board, sq(r, f))) break;
+        set_bit(&attacks, sq(r, f));
+        if (get_bit(board, sq(r, f))) break;
     }
     for (r = curRank + 1, f = curFile - 1; r <= 7 && f >= 0; ++r, --f) {
-        setBit(&attacks, sq(r, f));
-        if (getBit(board, sq(r, f))) break;
+        set_bit(&attacks, sq(r, f));
+        if (get_bit(board, sq(r, f))) break;
     }
     for (r = curRank - 1, f = curFile + 1; r >= 0 && f <= 7; --r, ++f) {
-        setBit(&attacks, sq(r, f));
-        if (getBit(board, sq(r, f))) break;
+        set_bit(&attacks, sq(r, f));
+        if (get_bit(board, sq(r, f))) break;
     }
     for (r = curRank - 1, f = curFile - 1; r >= 0 && f >= 0; --r, --f) {
-        setBit(&attacks, sq(r, f));
-        if (getBit(board, sq(r, f))) break;
+        set_bit(&attacks, sq(r, f));
+        if (get_bit(board, sq(r, f))) break;
     }
 
     return attacks;
@@ -199,16 +195,16 @@ U64 generateBishopAttacks(int square, U64 board) {
  *
  * square - the square that the rook is on
  */
-U64 maskRookAttacks(int square) {
+U64 mask_rook_attacks(int square) {
     U64 attacks = 0ULL;
-    int curRank = square / 8;
-    int curFile = square % 8;
+    int rank = square / 8;
+    int file = square % 8;
 
     int r, f;
-    for (r = curRank + 1, f = curFile; r <= 6; ++r) setBit(&attacks, sq(r, f));
-    for (r = curRank - 1, f = curFile; r >= 1; --r) setBit(&attacks, sq(r, f));
-    for (r = curRank, f = curFile + 1; f <= 6; ++f) setBit(&attacks, sq(r, f));
-    for (r = curRank, f = curFile - 1; f >= 1; --f) setBit(&attacks, sq(r, f));
+    for (r = rank + 1, f = file; r <= 6; ++r) set_bit(&attacks, sq(r, f));
+    for (r = rank - 1, f = file; r >= 1; --r) set_bit(&attacks, sq(r, f));
+    for (r = rank, f = file + 1; f <= 6; ++f) set_bit(&attacks, sq(r, f));
+    for (r = rank, f = file - 1; f >= 1; --f) set_bit(&attacks, sq(r, f));
 
     return attacks;
 }
@@ -218,48 +214,55 @@ U64 maskRookAttacks(int square) {
  * square - the square that the rook is on
  * board - the pieces on the board around the rook
  */
-U64 generateRookAttacks(int square, U64 board) {
+U64 generate_rook_attacks(int square, U64 board) {
     U64 attacks = 0ULL;
-    int curRank = square / 8;
-    int curFile = square % 8;
+    int rank = square / 8;
+    int file = square % 8;
 
     int r, f;
-    for (r = curRank + 1, f = curFile; r <= 7; ++r) {
-        setBit(&attacks, sq(r, f));
-        if (getBit(board, sq(r, f))) break;
+    for (r = rank + 1, f = file; r <= 7; ++r) {
+        set_bit(&attacks, sq(r, f));
+        if (get_bit(board, sq(r, f))) break;
     }
-    for (r = curRank - 1, f = curFile; r >= 0; --r) {
-        setBit(&attacks, sq(r, f));
-        if (getBit(board, sq(r, f))) break;
+    for (r = rank - 1, f = file; r >= 0; --r) {
+        set_bit(&attacks, sq(r, f));
+        if (get_bit(board, sq(r, f))) break;
     }
-    for (r = curRank, f = curFile + 1; f <= 7; ++f) {
-        setBit(&attacks, sq(r, f));
-        if (getBit(board, sq(r, f))) break;
+    for (r = rank, f = file + 1; f <= 7; ++f) {
+        set_bit(&attacks, sq(r, f));
+        if (get_bit(board, sq(r, f))) break;
     }
-    for (r = curRank, f = curFile - 1; f >= 0; --f) {
-        setBit(&attacks, sq(r, f));
-        if (getBit(board, sq(r, f))) break;
+    for (r = rank, f = file - 1; f >= 0; --f) {
+        set_bit(&attacks, sq(r, f));
+        if (get_bit(board, sq(r, f))) break;
     }
 
     return attacks;
 }
 
-void initAttackTables() {
+void init_attack_tables() {
     for (int square = 0; square < 64; ++square) {
         // pawn attack tables
-        pawnAttacksTable[white][square] = maskPawnAttacks(square, white);
-        pawnAttacksTable[black][square] = maskPawnAttacks(square, black);
+        pawn_attacks_table[white][square] = mask_pawn_attacks(square, white);
+        pawn_attacks_table[black][square] = mask_pawn_attacks(square, black);
         // knight attack tables
-        knightAttacksTable[square] = maskKnightAttacks(square);
+        knight_attacks_table[square] = mask_knight_attacks(square);
         // bishop attack tables
         // rook attack tables
         // queen attack tables
         // king attack tables
-        kingAttacksTable[square] = maskKingAttacks(square);
+        king_attacks_table[square] = mask_king_attacks(square);
     }
 }
 
 int main(void) {
-    initAttackTables();
+    init_attack_tables();
+
+    U64 board = 0ULL;
+    set_bit(&board, e2);
+    set_bit(&board, e7);
+    set_bit(&board, b4);
+    set_bit(&board, g4);
+    print_bitboard(generate_rook_attacks(e4, board));
     return 0;
 }
